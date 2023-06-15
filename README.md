84c6c271ac4339948bc616300bcba798a55cdf2c
王洪亮 <w505957125@aliyun.com>
2023-06-15 20:16:06 +0800

    add tomcat-embed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-demo
+
+```bash
+--add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED
+```
+
+`Add dependencies with "provided" scope to classpath`
---
+++ src/main/java/com/whl/spring/Bootstrap.java
@@ -0,0 +1,46 @@
+package com.whl.spring;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class Bootstrap {
+    private static final int PORT = 8080;
+
+    private static final String URI_ENCODING = "UTF-8";
+
+    private static final String CONTEXT_PATH = "/spring";
+
+    private static final String WEBAPP = "src/main/webapp";
+
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+
+        File baseDir = createTempDir("tomcat");
+        tomcat.setBaseDir(baseDir.getAbsolutePath());
+
+        Connector connector = new Connector();
+        connector.setPort(PORT);
+        connector.setURIEncoding(URI_ENCODING);
+        tomcat.getService().addConnector(connector);
+
+        File docBase = new File(WEBAPP);
+        tomcat.addWebapp(CONTEXT_PATH, docBase.getAbsolutePath());
+
+        tomcat.getHost().setAutoDeploy(false);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+    private static File createTempDir(String prefix) throws IOException {
+        File tempDir = File.createTempFile(prefix + ".", "." + PORT);
+        tempDir.delete();
+        tempDir.mkdir();
+        tempDir.deleteOnExit();
+        return tempDir;
+    }
+
+}
